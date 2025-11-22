cd6a00e14a6541433d5c3d3875da30f8891340b5
Muhammet Madraimov <mmadraimov@gmail.com>
2025-11-22 13:31:19 +0200

    fix lint warnings

--- terminal/terminal.go
+++ terminal/terminal.go
@@ -10,10 +10,10 @@ type state struct {
 	// Dummy struct for unsupported platforms
 }
 
-func makeRaw(fd int) (*state, error) {
+func makeRaw(int) (*state, error) {
 	return nil, fmt.Errorf("terminal raw mode not supported on this platform")
 }
 
-func restore(fd int, state *state) error {
+func restore(int, *state) error {
 	return fmt.Errorf("terminal restore not supported on this platform")
 }
